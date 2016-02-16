7e666b05ea573df3a208119fe9c5be605d0f3eca
Martin Mena <mjmena.dev@gmail.com>
2016-02-15 21:37:15 -0500

    Initial commit for Bird: The Game (working title)

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    public float acceleration;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Rigidbody2D body = GetComponent<Rigidbody2D>();
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            body.velocity = new Vector2(body.velocity.x, acceleration);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            body.velocity = new Vector2(-acceleration, body.velocity.y);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            body.velocity = new Vector2(body.velocity.x, -acceleration);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            body.velocity = new Vector2(acceleration, body.velocity.y);
+        }
+    }
+}
