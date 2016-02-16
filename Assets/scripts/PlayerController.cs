ad4e02133764641d4036a7dbba5bff13f61d7ce3
Tropical Mage <tropicalmage@gmail.com>
2016-02-15 22:49:51 -0500

    added rude_dude

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -2,32 +2,39 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-    public float acceleration;
+	private Rigidbody2D body;
+
+	public float speed;
 
 	// Use this for initialization
 	void Start () {
-	
+		body = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Rigidbody2D body = GetComponent<Rigidbody2D>();
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            body.velocity = new Vector2(body.velocity.x, acceleration);
-        }
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            body.velocity = new Vector2(-acceleration, body.velocity.y);
-        }
+		if (Input.GetKeyDown (KeyCode.W)) {
+			body.velocity = new Vector2(body.velocity.x, speed);
+		} else if (Input.GetKeyUp(KeyCode.W)) {
+			body.velocity = new Vector2 (body.velocity.x, 0);
+		}
+
+        if (Input.GetKeyDown(KeyCode.A)) {
+			body.velocity = new Vector2(-speed, body.velocity.y);
+		} else if (Input.GetKeyUp(KeyCode.A)) {
+			body.velocity = new Vector2(0, body.velocity.y);
+		}
         
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            body.velocity = new Vector2(body.velocity.x, -acceleration);
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            body.velocity = new Vector2(acceleration, body.velocity.y);
-        }
+        if (Input.GetKeyDown(KeyCode.S)) {
+			body.velocity = new Vector2(body.velocity.x, -speed);
+		} else if (Input.GetKeyUp(KeyCode.S)) {
+			body.velocity = new Vector2(body.velocity.x, 0);
+		}
+
+        if (Input.GetKeyDown(KeyCode.D)) {
+			body.velocity = new Vector2(speed, body.velocity.y);
+		} else if (Input.GetKeyUp(KeyCode.D)) {
+			body.velocity = new Vector2(0, body.velocity.y);
+		}
     }
 }
