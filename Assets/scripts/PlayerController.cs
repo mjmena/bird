38821821fd2e8c647387cceb9c75c96c8685f7c0
Tropical Mage <tropicalmage@gmail.com>
2016-02-16 19:49:01 -0500

    Attached animation to player

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -6,13 +6,17 @@ public class PlayerController : MonoBehaviour {
 
 	public float speed;
 
+	Animator anim;
+
 	// Use this for initialization
 	void Start () {
 		body = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
 		if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
 			body.velocity = new Vector2(body.velocity.x, speed);
 		}
@@ -36,5 +40,8 @@ public class PlayerController : MonoBehaviour {
 		if (!(Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))) {
 			body.velocity = new Vector2(body.velocity.x, 0);
 		}
+
+
+		anim.SetFloat ("speed", Mathf.Abs (Mathf.Pow (body.velocity.x, 2) + Mathf.Pow (body.velocity.y, 2)));
 	}
 }
