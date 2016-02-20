e27abf8bf644b188bfa610256cfa9501cc047ac9
Martin Mena <mjmena.dev@gmail.com>
2016-02-20 02:18:04 -0500

    Complete merge

--- Assets/scripts/UI/EleformWheel.cs
+++ Assets/scripts/UI/EleformWheel.cs
@@ -5,7 +5,6 @@ public class EleformWheel : MonoBehaviour {
 	public GameObject player;
 
 	void Update () {
-		Debug.Log (player.GetComponent<CombatController> ().GetNextElement ());
 		GetComponent<Animator> ().SetInteger ("element_number", player.GetComponent<CombatController> ().GetNextElement ());
 	}
 }
